451fda336d147dff57008d1ebdc0bab23ec1266b
Vladimir Sernatsky <byport1112@gmail.com>
2017-01-22 14:38:49 +0300

    cool model, refactoring, reconfigurangdgfhth

---
+++ src/app/models/model.js
@@ -0,0 +1,72 @@
+'use strict';
+
+import LSDB from '../vendor/lsdb.js';
+import $ from 'jquery';
+
+export default class Model {
+    constructor(id) {
+        if (typeof(id) !== 'undefined') {
+            if (typeof(this.constructor.table.findWhere({id: id})) === 'undefined') throw new Error('No model with same id.');
+        } else {
+            this.constructor.table.insert(
+                Object.assign({}, this.constructor.image)
+            );
+            id = this.constructor.table._lastId;
+        }
+
+        Object.defineProperty(this, 'id', {
+            value: id,
+            writeable: false,
+            configurable: false
+        });
+
+        for (let prop in this.constructor.image) {
+            Object.defineProperty(this, prop, {
+                get: () => {
+                    return this.constructor.table.findWhere({
+                        id: this.id
+                    })[prop];
+                },
+                set: (value) => {
+                    let row = {id: this.id};
+                    row[prop] = value;
+                    this.constructor.table.update(row, 'id');
+                }
+            });
+        }
+    }
+
+    static all() {
+        return $.map(
+            this.table.all(),
+            (row) => {
+                return new this(row.id);
+            }
+        );
+    }
+
+    static where(props) {
+        return $.map(
+            this.table.where(props),
+            (row) => {
+                return new this(row.id);
+            }
+        );
+    }
+
+    static setTable(name) {
+        Object.defineProperty(this, 'table', {
+            value: new LSDB.Table(name),
+            writeable: false,
+            configurable: false
+        });
+    }
+
+    static setImage(image) {
+        Object.defineProperty(this, 'image', {
+            value: image,
+            writeable: false,
+            configurable: false
+        });
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,46 @@
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: './src/js/app.js',
+    entry: {
+        app: './src/app/main.js'
+    },
     output: {
         path: './bin',
-        filename: 'app.bundle.js'
+        filename: '[name].bundle.js',
+    },
+    resolve: {
+        extentions: ['', '.js', '.jsx']
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        },
-        {
-            test: /\.scss$/,
-            loaders: ['style-loader', 'css-loader', 'sass-loader'],
-            exclude: /node_modules/
-        }
-	    ]
-    }
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            }
+	]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.bundle.css', { allChunks: true }),
+        new HtmlPlugin(/*{
+            template: './src/index.html'
+        }*/)
+    ]
  };
